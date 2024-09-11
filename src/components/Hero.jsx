bfa335f71c61c53ab30c8635eba13a1d25dc85ff
Farothtc <mvkirimi@gmail.com>
2024-09-11 20:11:06 +0300

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+export function Hero() {
+  return (
+    <section className="hero">
+      <img src="./image-cluster.png" className="hero-image" />
+      <div className="hero-text">
+        <h1 className="hero-header">Online Experiences</h1>
+        <h3 className="hero-text">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </h3>
+      </div>
+    </section>
+  );
+}
