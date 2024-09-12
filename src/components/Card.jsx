8e937662574522e3036f66a6a04d86bce1f3ee8c
Farothtc <mvkirimi@gmail.com>
2024-09-12 13:32:35 +0300

    Initial commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,1 +1,17 @@
-export function Card() {}
+export function Card() {
+  return (
+    <div className="card">
+      <img src="./katie.png" className="card-image" />
+      <div className="card-line">
+        <img src="./star.png" />
+        <span>5.0</span>
+        <span className="gray">(6) •</span>
+        <span className="gray">USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p>
+        <strong>From $136</strong>/ person
+      </p>
+    </div>
+  );
+}
