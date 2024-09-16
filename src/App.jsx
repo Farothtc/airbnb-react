b2b554e5e8f23ac4e9b95e997abecb902d5fa969
Farothtc <mvkirimi@gmail.com>
2024-09-16 13:43:39 +0300

    .map() method is used for rendering several different card components

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,29 @@ import { Hero } from "./components/Hero";
 import { Card } from "./components/Card";
 import { Joke } from "./components/Joke";
 import jokesData from "../jokesData";
+import data from "../data";
 
 function App() {
-  const jokeElements = jokesData.map((joke) => (
-    <Joke setup={joke.setup} punchline={joke.punchline} />
+  const dataElements = data.map((e) => (
+    <Card
+      id={e.id}
+      title={e.title}
+      img={e.coverImg}
+      price={e.price}
+      rating={e.stats.rating}
+      reviewCount={e.stats.reviewCount}
+      location={e.location}
+    />
   ));
+  // const jokeElements = jokesData.map((joke) => (
+  //   <Joke setup={joke.setup} punchline={joke.punchline} />
+  // ));
 
   return (
     <>
       <Navbar />
       <Hero />
-      <Card
-        img="katie.png"
-        rating="5.0"
-        reviewCount={6}
-        country="Turkey"
-        title="Life Lessons with Katie Zarefes"
-        price={136}
-      />
-      {jokeElements}
+      {dataElements}
     </>
   );
 }
